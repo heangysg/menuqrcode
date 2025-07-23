3c460490f3aa1d6d945992e4ecc1ece416f0ddb7
Emma Heang <kimheak868@gmail.com>
2025-07-23 09:12:31 +0700

    Final code updates before deployment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,7 +2,7 @@
 
 // IMPORTANT: Change this to your deployed Render backend URL when deploying!
 // For local development, it should typically be http://localhost:5000/api
-const API_BASE_URL = 'https://menu-qr-61oz.onrender.com/api'; 
+const API_BASE_URL = 'https://menuqrcode.onrender.com//api'; 
 // const API_BASE_URL = 'http://localhost:5000/api'; 
 
 
